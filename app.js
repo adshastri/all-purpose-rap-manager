fd92daf2651398a79ee3092cf2f1ed071be91d75
Aditya Shastri <ad.shastri1@gmail.com>
2017-09-16 23:19:47 -0400

    use env var for heroku

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var CONFIG = require('./config.js');
+var CONFIG = JSON.parse(process.env.CONFIG) || require('./config.js');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
